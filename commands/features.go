368e469d6286f4fba222fb2fa466caf3e1a67d45
Andrew Brampton <bramp@google.com>
2018-01-22 17:02:31 -0800

    Change feature to only print the description of the field if it is known.

--- commands/features.go
+++ commands/features.go
@@ -34,21 +34,28 @@ func layoutTable(font *sfnt.Font, tag sfnt.Tag, name string) {
 
 		t := font.Table(tag).(*sfnt.TableLayout)
 		for _, script := range t.Scripts {
-			fmt.Printf("\tScript %q (%s):\n", script.Tag, script.String())
+			fmt.Printf("\tScript %q%s:\n", script.Tag, bracketString(script))
 
 			fmt.Printf("\t\tDefault Language:\n")
 			for _, feature := range script.DefaultLanguage.Features {
-				fmt.Printf("\t\t\tFeature %q (%s)\n", feature.Tag, feature.String())
+				fmt.Printf("\t\t\tFeature %q%s\n", feature.Tag, bracketString(feature))
 			}
 
 			for _, lang := range script.Languages {
-				fmt.Printf("\t\tLanguage %q (%s):\n", lang.Tag, lang.String())
+				fmt.Printf("\t\tLanguage %q%s:\n", lang.Tag, bracketString(lang))
 				for _, feature := range lang.Features {
-					fmt.Printf("\t\t\tFeature %q (%s)\n", feature.Tag, feature.String())
+					fmt.Printf("\t\t\tFeature %q%s\n", feature.Tag, bracketString(feature))
 				}
 			}
 		}
 	} else {
 		fmt.Printf("No %s\n", name)
 	}
+}
+
+func bracketString(o fmt.Stringer) string {
+	if s := o.String(); s != "" {
+		return fmt.Sprintf(" (%s)", s)
+	}
+	return ""
 }
